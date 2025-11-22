2047e921ce402b252fa1361d02a34ea37d01f43f
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-11-22 12:21:54 -0800

    update with infra

--- 111525/infra/02-subnets.tf
+++ 111525/infra/02-subnets.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_a" {
 
   cidr_block              = "10.10.1.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true         # allow public IPs to be assigned 
+  map_public_ip_on_launch = true # allow public IPs to be assigned 
 
   tags = {
     Name = "public-subnet-us-west-2a"
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.10.2.0/24"
   availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true         # allow public IPs to be assigned 
+  map_public_ip_on_launch = true # allow public IPs to be assigned 
 
   tags = {
     Name = "public-subnet-us-west-2b"
@@ -34,7 +34,7 @@ resource "aws_subnet" "public_c" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.10.3.0/24"
   availability_zone       = data.aws_availability_zones.available.names[2]
-  map_public_ip_on_launch = true         # allow public IPs to be assigned 
+  map_public_ip_on_launch = true # allow public IPs to be assigned 
 
   tags = {
     Name = "public-subnet-us-west-2c"
--- 111525/infra/06a-sg-web-app.tf
+++ 111525/infra/06a-sg-web-app.tf
@@ -1,45 +1,36 @@
-# Web Server Security Group
-resource "aws_security_group" "web_server" {
-  name        = "web-server-sg"
-  description = "Allow HTTP and SSH for web server"
+# for ASG EC2 instances
+
+resource "aws_security_group" "web_tier" {
+  name        = "allow_http_ssh_web_app"
+  description = "Allow inbound HTTP and SSH for web apps in web tier"
   vpc_id      = aws_vpc.main.id
+  # <resource type>.<resource local name>.<attribute type> 
 
   tags = {
-    Name = "web-server-sg"
-    Tier = "web"
+    Name = "allow-http-ssh-web-app"
   }
-
-
 }
 
-resource "aws_vpc_security_group_ingress_rule" "web_server_http" {
-  security_group_id = aws_security_group.web_server.id
-  description       = "HTTP from internet"
-  # if traffic from LB then can this be improved?
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+resource "aws_vpc_security_group_ingress_rule" "web_tier_allow_http" {
+  security_group_id = aws_security_group.web_tier.id
+  # ALB SG ID goes here (ALB is the source of HTTP traffic for web apps)
+  referenced_security_group_id = aws_security_group.web_load_balancer.id
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
 }
 
-
-resource "aws_vpc_security_group_ingress_rule" "web_server_ssh" {
-  security_group_id = aws_security_group.web_server.id
-  description       = "SSH from internet"
-  cidr_ipv4         = "0.0.0.0/0"
+resource "aws_vpc_security_group_ingress_rule" "web_tier_allow_ssh" {
+  security_group_id = aws_security_group.web_tier.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_egress_rule" "web_server_egress" {
-  security_group_id = aws_security_group.web_server.id
-  description       = "Allow all outbound traffic"
+resource "aws_vpc_security_group_egress_rule" "web_tier_allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.web_tier.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
-
-
-
-
 
