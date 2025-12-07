119f6c9508fd60500974d95cb0bfc659caf506e7
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-12-07 13:29:12 -0800

    add dependency

--- 120625/02-bucket-config.tf
+++ 120625/02-bucket-config.tf
@@ -39,6 +39,8 @@ resource "aws_s3_bucket_policy" "public_access" {
       }
     ]
   })
+   # add this explict dependency, dependency chain issues occur intermittenly 
+  depends_on = [ aws_s3_bucket_public_access_block.website ]
 }
 
 
